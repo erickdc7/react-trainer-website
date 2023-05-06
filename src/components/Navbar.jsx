b5a03bcb278a56002bea4ecb63049edf4e39377b
ErickDC <erickdiazc717@gmail.com>
2023-05-06 17:36:21 -0500

    links added to Navbar.jsx component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link, NavLink } from 'react-router-dom'
 import './navbar.css'
 import Logo from '../images/logo.png'
+import { links } from '../data'
 
 const Navbar = () => {
     return (
@@ -9,6 +10,20 @@ const Navbar = () => {
                 <Link to='/' className='logo'>
                     <img src={Logo} alt="nav logo" />
                 </Link>
+
+                <ul className="nav__links">
+                    {
+                        links.map(({ name, path }, index) => {
+                            return (
+                                <li>
+                                    <NavLink to={path}>{name}</NavLink>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+
+                
             </div>
         </nav>
     )
