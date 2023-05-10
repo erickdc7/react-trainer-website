c1915f9299322f672ff381e1aa501f9cf431497b
ErickDC <erickdiazc717@gmail.com>
2023-05-10 12:16:34 -0500

    programs__wrapper updated

--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FaCrown } from 'react-icons/fa'
 import SectionHead from './SectionHead'
-
+import { programs } from '../data'
 
 const Programs = () => {
     return (
@@ -14,7 +14,13 @@ const Programs = () => {
             </div>
 
             <div className="programs__wrapper">
-                
+                {
+                    programs.map(({ id, icon, title, info, path }) => {
+                        return (
+                            <h4>Hi</h4>
+                        )
+                    })
+                }
             </div>
         </section>
     )
