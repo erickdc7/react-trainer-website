110e5f47e7298a5d8e98bb892bd08c69d191d756
ErickDC <erickdiazc717@gmail.com>
2023-05-10 12:07:39 -0500

    Programs.jsx added to Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './home.css'
 import MainHeader from '../../components/MainHeader'
+import Programs from '../../components/Programs'
 
 const Home = () => {
     return (
         <>
             <MainHeader />
+            <Programs />
         </>
     )
 }
