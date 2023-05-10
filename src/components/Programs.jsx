aee344c64c691777cf4bded78353bee04e580b31
ErickDC <erickdiazc717@gmail.com>
2023-05-10 12:09:35 -0500

    SectionHead.jsx created

--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -6,9 +6,7 @@ const Programs = () => {
     return (
         <section className="programs">
             <div className="container programs__container">
-                <div className="programs__header">
-                    <span>{<FaCrown />}</span>
-                </div>
+                
             </div>
         </section>
     )
