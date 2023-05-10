95efc174577c5f82305f8a4078fca4262cd62119
ErickDC <erickdiazc717@gmail.com>
2023-05-10 12:43:12 -0500

    programs styles added to home.css

--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -14,23 +14,23 @@ const Programs = () => {
                     icon={<FaCrown />}
                     title="Programs"
                 />
-            </div>
 
-            <div className="programs__wrapper">
-                {
-                    programs.map(({ id, icon, title, info, path }) => {
-                        return (
-                            <Card className="programs__program" key={id}>
-                                <span>{icon}</span>
-                                <h4>{title}</h4>
-                                <small>{info}</small>
-                                <Link to={path} className='btn sm'>
-                                    Learn more <AiFillCaretRight />
-                                </Link>
-                            </Card>
-                        )
-                    })
-                }
+                <div className="programs__wrapper">
+                    {
+                        programs.map(({ id, icon, title, info, path }) => {
+                            return (
+                                <Card className="programs__program" key={id}>
+                                    <span>{icon}</span>
+                                    <h4>{title}</h4>
+                                    <small>{info}</small>
+                                    <Link to={path} className='btn sm'>
+                                        Learn more <AiFillCaretRight />
+                                    </Link>
+                                </Card>
+                            )
+                        })
+                    }
+                </div>
             </div>
         </section>
     )
