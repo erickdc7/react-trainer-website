91f3ea1b4217e6c8a90e1ace2076b040fcaf1711
ErickDC <erickdiazc717@gmail.com>
2023-05-11 11:15:26 -0500

    values__left added to Values.jsx

--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Image from '../images/values.jpg'
+import SectionHead from './SectionHead'
+import { GiCutDiamond } from 'react-icons/gi'
+import { values } from '../data'
+import Card from '../UI/Card'
 
 const Values = () => {
     return (
-        <div>Values</div>
+        <section className='values'>
+            <div className="container values__container">
+                <div className="values__left">
+                    <div className="values__image">
+                        <img src={Image} alt="values" />
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
