efd193fcf840180266f28acde9c5f4200dad12e7
ErickDC <erickdiazc717@gmail.com>
2023-05-13 18:08:28 -0500

    added toggle functionality

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { AiOutlinePlus } from 'react-icons/ai'
 import { AiOutlineMinus } from 'react-icons/ai'
 
 const FAQ = ({ question, answer }) => {
+    const [isAnswerShowing, setIsAnswerShowing] = useState(false)
+
     return (
-        <article className="faq">
+        <article className="faq" onClick={() => setIsAnswerShowing(prev => !prev)}>
             <div>
                 <h4>{question}</h4>
                 <button className="faq__icon">
-                    <AiOutlinePlus />
+                    {
+                        isAnswerShowing
+                            ? <AiOutlineMinus />
+                            : <AiOutlinePlus />
+                    }
                 </button>
             </div>
 
-            <p>{answer}</p>
+            {
+                isAnswerShowing && (
+                    <p>{answer}</p>
+                )
+            }
         </article>
     )
 }
