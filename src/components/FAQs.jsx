d905ead14e00840b61c81cba0005d784cd0086aa
ErickDC <erickdiazc717@gmail.com>
2023-05-13 17:32:44 -0500

    FAQ.jsx added to FAQs.jsx

--- src/components/FAQs.jsx
+++ src/components/FAQs.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import SectionHead from './SectionHead'
 import { FaQuestion } from 'react-icons/fa'
 import { faqs } from '../data'
+import FAQ from './FAQ'
 
 const FAQs = () => {
     return (
