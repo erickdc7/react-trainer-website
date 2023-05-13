9ef3b1eee0a5568e3ee004c41047324f1eef8540
ErickDC <erickdiazc717@gmail.com>
2023-05-13 17:38:25 -0500

    FAQ.jsx updated

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { AiOutlinePlus } from 'react-icons/ai'
+import { AiOutlineMinus } from 'react-icons/ai'
 
-const FAQ = () => {
+const FAQ = ({ question, answer }) => {
     return (
-        <div>FAQ</div>
+        <article className="faq">
+            <div>
+                <h4>{question}</h4>
+                <button className="faq__icon">
+                    <AiOutlinePlus />
+                </button>
+            </div>
+        </article>
     )
 }
 
