73812436ce570c749e2a3ecf3c758f4c89e43fdc
ErickDC <erickdiazc717@gmail.com>
2023-05-14 12:19:47 -0500

    container testimonials__container added in Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SectionHead from './SectionHead'
+import { ImQuotesLeft } from 'react-icons/im'
+import Card from '../UI/Card'
+import { IoIosArrowDropleftCircle, IoIosArrowDroprightCircle } from 'react-icons/io'
+import { testimonials } from '../data'
 
 const Testimonials = () => {
+    const [index, setIndex] = useState(0)
+    const { name, quote, job, avatar } = testimonials[index]
+
     return (
-        <div>Testimonials</div>
+        <section className="testimonials">
+            <div className="container testimonials__container">
+                <SectionHead
+                    icon={<ImQuotesLeft />}
+                    title="Testimonials"
+                />
+
+                <Card className="testimonial">
+                    <div className="testimonial__avatar">
+                        <img src={avatar} alt={name} />
+                    </div>
+                    <p className="testimonial__quote">
+                        {`"${quote}"`}
+                    </p>
+                    <h5>{name}</h5>
+                    <small className='testimonial__title'>{job}</small>
+                </Card>
+            </div>
+        </section>
     )
 }
 
