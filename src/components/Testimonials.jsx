6430cbe8a1469f47b1a23cfc2ae2f5bde87006b4
ErickDC <erickdiazc717@gmail.com>
2023-05-14 12:58:40 -0500

    prevTestimonialHandler and nextTestimonialHandler functions added

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -9,6 +9,22 @@ const Testimonials = () => {
     const [index, setIndex] = useState(0)
     const { name, quote, job, avatar } = testimonials[index]
 
+    const prevTestimonialHandler = () => {
+        setIndex(prev => prev - 1)
+
+        if (index <= 0) {
+            setIndex(testimonials.length - 1)
+        }
+    }
+
+    const nextTestimonialHandler = () => {
+        setIndex(prev => prev + 1)
+
+        if (index >= testimonials.length - 1) {
+            setIndex(0)
+        }
+    }
+
     return (
         <section className="testimonials">
             <div className="container testimonials__container">
@@ -30,11 +46,11 @@ const Testimonials = () => {
                 </Card>
 
                 <div className="testimonials__btn-container">
-                    <button className="testimonials__btn">
+                    <button className="testimonials__btn" onClick={prevTestimonialHandler}>
                         <IoIosArrowDropleftCircle />
                     </button>
 
-                    <button className="testimonials__btn">
+                    <button className="testimonials__btn" onClick={nextTestimonialHandler}>
                         <IoIosArrowDroprightCircle />
                     </button>
                 </div>
