3c46e828a7eaa55249349f2602eddc7abf8ec0de
ErickDC <erickdiazc717@gmail.com>
2023-05-15 12:24:17 -0500

    Footer.jsx updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../images/logo.png'
+import { FaLinkedin, FaFacebook } from 'react-icons/fa'
+import { AiOutlineTwitter, AiFillInstagram } from 'react-icons/ai'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer>
+            <div className="container footer__container">
+                <article>
+                    <Link to="/" className="logo">
+                        <img src={Logo} alt="footer logo" />
+                    </Link>
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque aperiam reiciendis sapiente tenetur repellendus aliquid, modi maxime suscipit doloribus optio.
+                    </p>
+                </article>
+            </div>
+        </footer>
     )
 }
 
