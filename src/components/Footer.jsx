8e00186f948c05366ad52c8c39e52e4e1ccb0e45
ErickDC <erickdiazc717@gmail.com>
2023-05-15 12:30:17 -0500

    permalinks changed in Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -47,23 +47,20 @@ const Footer = () => {
                 </article>
 
                 <article>
-                    <h4>Permalinks</h4>
+                    <h4>Insights</h4>
 
-                    <Link to="/about">About</Link>
-                    <Link to="/plans">Plans</Link>
-                    <Link to="/trainers">Trainers</Link>
-                    <Link to="/gallery">Gallery</Link>
-                    <Link to="/contact">Contact</Link>
+                    <Link to="/s">Blog</Link>
+                    <Link to="/s">Case Studies</Link>
+                    <Link to="/s">Events</Link>
+                    <Link to="/s">Communities</Link>
+                    <Link to="/s">FAQs</Link>
                 </article>
 
                 <article>
-                    <h4>Permalinks</h4>
+                    <h4>Get In Touch</h4>
 
-                    <Link to="/about">About</Link>
-                    <Link to="/plans">Plans</Link>
-                    <Link to="/trainers">Trainers</Link>
-                    <Link to="/gallery">Gallery</Link>
-                    <Link to="/contact">Contact</Link>
+                    <Link to="/contact">Contact Us</Link>
+                    <Link to="/s">Support</Link>
                 </article>
             </div>
 
