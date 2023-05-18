d4320029aed5d4402e73dad6332db1486657a4d4
ErickDC <erickdiazc717@gmail.com>
2023-05-18 11:44:38 -0500

    Header.jsx updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
-const Header = () => {
+const Header = ({ title, image, children }) => {
     return (
-        <div>Header</div>
+        <header className='header'>
+            <div className="header__container">
+                <div className="header__container-bg">
+                    <img src={image} alt="header background" />
+                </div>
+
+                <div className="header__content">
+                    <h2>{title}</h2>
+                    <p>{children}</p>
+                </div>
+            </div>
+        </header>
     )
 }
 
