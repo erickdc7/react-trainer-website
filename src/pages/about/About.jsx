fd40f231f7aabea6284bfe88bb9c009c7222a15c
ErickDC <erickdiazc717@gmail.com>
2023-05-18 11:48:18 -0500

    About.jsx updated

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './about.css'
+import Header from '../../components/Header'
+import HeaderImage from '../../images/header_bg_1.jpg'
 
 const About = () => {
     return (
-        <div>About</div>
+        <>
+            <Header
+                title="About Us"
+                image={HeaderImage}
+            >
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe distinctio consectetur eos facilis odio dicta sit non consequuntur harum sapiente.
+            </Header>
+        </>
     )
 }
 
