b4926ce47d8a087c7a2c5ca02e9cf643c8c2a959
ErickDC <erickdiazc717@gmail.com>
2023-05-18 12:13:54 -0500

    about__mission section added to About.jsx

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -4,6 +4,7 @@ import Header from '../../components/Header'
 import HeaderImage from '../../images/header_bg_1.jpg'
 import StoryImage from '../../images/about1.jpg'
 import VisionImage from '../../images/about2.jpg'
+import MissionImage from '../../images/about3.jpg'
 
 const About = () => {
     return (
@@ -47,6 +48,27 @@ const About = () => {
 
                     <div className="about__section-image">
                         <img src={VisionImage} alt="our vision" />
+                    </div>
+                </div>
+            </section>
+
+            <section className="about__mission">
+                <div className="container about__mission-container">
+                    <div className="about__section-image">
+                        <img src={MissionImage} alt="our mission" />
+                    </div>
+
+                    <div className="about__section-content">
+                        <h1>Our Mission</h1>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, doloremque dolor cumque tempore quibusdam repellendus ut ab facere incidunt beatae maxime et ipsam harum esse quod deserunt voluptatem quae numquam.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque inventore minima accusantium doloribus corporis? Doloremque, sed nostrum. At cupiditate repellat harum rerum vero amet facilis!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos illum recusandae, dignissimos magni similique magnam.
+                        </p>
                     </div>
                 </div>
             </section>
