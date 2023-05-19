3d8e015b3b7545b78d9184332eca2db77df5d394
ErickDC <erickdiazc717@gmail.com>
2023-05-19 10:36:41 -0500

    Header added to Contact.jsx

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './contact.css'
+import Header from '../../components/Header'
+import HeaderImage from '../../images/header_bg_2.jpg'
+import { BsMessenger } from 'react-icons/bs'
+import { IoLogoWhatsapp } from 'react-icons/io'
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <>
+            <Header title="Get In Touch" image={HeaderImage}>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus nihil blanditiis facere iste pariatur voluptate praesentium? Hic aliquid delectus repellat.
+            </Header>
+        </>
     )
 }
 
