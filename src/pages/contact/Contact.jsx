1ce1f1f5d908fa39d6bed0274cbb4ce9c360b11a
ErickDC <erickdiazc717@gmail.com>
2023-05-19 10:41:47 -0500

    section contact added to Contact.jsx

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './contact.css'
 import Header from '../../components/Header'
 import HeaderImage from '../../images/header_bg_2.jpg'
+import { MdEmail } from 'react-icons/md'
 import { BsMessenger } from 'react-icons/bs'
 import { IoLogoWhatsapp } from 'react-icons/io'
 
@@ -11,6 +12,24 @@ const Contact = () => {
             <Header title="Get In Touch" image={HeaderImage}>
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus nihil blanditiis facere iste pariatur voluptate praesentium? Hic aliquid delectus repellat.
             </Header>
+
+            <section className="contact">
+                <div className="container contact__container">
+                    <div className="contact__wrapper">
+                        <a href="#" target='_blank' rel='noreferrer noopener'>
+                            <MdEmail />
+                        </a>
+
+                        <a href="#" target='_blank' rel='noreferrer noopener'>
+                            <BsMessenger />
+                        </a>
+
+                        <a href="#" target='_blank' rel='noreferrer noopener'>
+                            <IoLogoWhatsapp />
+                        </a>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
