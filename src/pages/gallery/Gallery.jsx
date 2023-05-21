e577fcc553df10623c9950cac9bee16409fbe46c
ErickDC <erickdiazc717@gmail.com>
2023-05-21 12:35:53 -0500

    functionalities added in Gallery.jsx

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -4,7 +4,13 @@ import Header from '../../components/Header'
 import HeaderImage from '../../images/header_bg_3.jpg'
 
 const Gallery = () => {
-    
+    const galleryLength = 15,
+        images = []
+
+    for (let i = 1; i <= galleryLength; i++) {
+        images.push(require(`../../images/gallery${i}`))
+    }
+
     return (
         <>
             <Header title="Our Gallery" image={HeaderImage}>
