d35570fbfa7ead3c80c74fa900c9dce7f406614f
ErickDC <erickdiazc717@gmail.com>
2023-05-21 12:34:51 -0500

    Gallery.jsx updated

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './gallery.css'
+import Header from '../../components/Header'
+import HeaderImage from '../../images/header_bg_3.jpg'
 
 const Gallery = () => {
+    
     return (
-        <div>Gallery</div>
+        <>
+            <Header title="Our Gallery" image={HeaderImage}>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis magni nulla vel unde optio laborum animi repudiandae totam, cum obcaecati.
+            </Header>
+        </>
     )
 }
 
