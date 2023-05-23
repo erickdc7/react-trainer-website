05293a26a423abdce5958c830f625bcf5a19baae
ErickDC <erickdiazc717@gmail.com>
2023-05-23 11:14:43 -0500

    Header added to Plans.jsx

--- src/pages/plans/Plans.jsx
+++ src/pages/plans/Plans.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './plans.css'
+import Header from '../../components/Header'
+import HeaderImage from '../../images/header_bg_4.jpg'
+import Card from '../../UI/Card'
+import { plans } from '../../data'
 
 const Plans = () => {
     return (
-        <div>Plans</div>
+        <>
+            <Header title="Membership Plans" image={HeaderImage}>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem quia cupiditate eaque officiis nostrum! Obcaecati ullam ut perspiciatis omnis doloribus.
+            </Header>
+        </>
     )
 }
 
