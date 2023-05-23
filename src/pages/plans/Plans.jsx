0b7bfd80629ec49ec9fc3d45112a00e5ad077f47
ErickDC <erickdiazc717@gmail.com>
2023-05-23 11:15:02 -0500

    plans section added to Plans.jsx

--- src/pages/plans/Plans.jsx
+++ src/pages/plans/Plans.jsx
@@ -11,6 +11,35 @@ const Plans = () => {
             <Header title="Membership Plans" image={HeaderImage}>
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem quia cupiditate eaque officiis nostrum! Obcaecati ullam ut perspiciatis omnis doloribus.
             </Header>
+
+            <section className="plans">
+                <div className="container plans__container">
+                    {
+                        plans.map(({ id, name, desc, price, features }) => {
+                            return (
+                                <Card key={id} className="plan">
+                                    <h3>{name}</h3>
+                                    <small>{desc}</small>
+                                    <h1>{`$${price}`}</h1><h2>/mo</h2>
+                                    <h4>Features</h4>
+                                    {
+                                        features.map(({ feature, available }, index) => {
+                                            return (
+                                                <p key={index} className={!available ? 'disabled' : ''}>
+                                                    {feature}
+                                                </p>
+                                            )
+                                        })
+                                    }
+                                    <button className='btn lg'>
+                                        Choose Plan
+                                    </button>
+                                </Card>
+                            )
+                        })
+                    }
+                </div>
+            </section>
         </>
     )
 }
