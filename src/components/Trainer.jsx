e0adb9bd8f88edb565de71abc981a42718ed567e
ErickDC <erickdiazc717@gmail.com>
2023-05-24 10:48:43 -0500

    Trainer.jsx updated

--- src/components/Trainer.jsx
+++ src/components/Trainer.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Card from '../UI/Card'
 
-const Trainer = () => {
+const Trainer = ({ image, name, job, socials }) => {
     return (
-        <div>Trainer</div>
+        <Card className="trainer">
+            <div className="trainer__img">
+                <img src={image} alt={name} />
+            </div>
+            <h3>{name}</h3>
+            <p>{job}</p>
+        </Card>
     )
 }
 
