d1504ee95784741a4bafec33786123641ec8ae5e
ErickDC <erickdiazc717@gmail.com>
2023-05-24 10:43:26 -0500

    Header added in Trainers.jsx

--- src/pages/trainers/Trainers.jsx
+++ src/pages/trainers/Trainers.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './trainers.css'
+import Header from '../../components/Header'
+import HeaderImage from '../../images/header_bg_5.jpg'
+import { trainers } from '../../data'
+import { BsInstagram } from 'react-icons/bs'
+import { AiOutlineTwitter } from 'react-icons/ai'
+import { FaFacebookF } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
 
 const Trainers = () => {
     return (
-        <div>Trainers</div>
+        <>
+            <Header title="Our Trainers" image={HeaderImage}>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio accusamus cum nihil quasi dolores molestias nisi veritatis adipisci fuga corrupti!
+            </Header>
+        </>
     )
 }
 
